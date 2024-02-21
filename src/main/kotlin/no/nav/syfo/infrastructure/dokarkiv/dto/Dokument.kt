8e1e6210044b6db2cc7cb82f1444c4eefe19bd26
Solgård <vetle.bu.solgard@nav.no>
2024-02-21 13:29:30 +0100

    IS-2093: Add dokarkiv client + skeleton for journalforing of varsler

---
+++ src/main/kotlin/no/nav/syfo/infrastructure/dokarkiv/dto/Dokument.kt
@@ -0,0 +1,24 @@
+package no.nav.syfo.infrastructure.dokarkiv.dto
+
+enum class BrevkodeType(
+    val value: String,
+)
+
+data class Dokument private constructor(
+    val brevkode: String,
+    val dokumentKategori: String? = null,
+    val dokumentvarianter: List<Dokumentvariant>,
+    val tittel: String? = null,
+) {
+    companion object {
+        fun create(
+            brevkode: BrevkodeType,
+            dokumentvarianter: List<Dokumentvariant>,
+            tittel: String? = null,
+        ) = Dokument(
+            brevkode = brevkode.value,
+            dokumentvarianter = dokumentvarianter,
+            tittel = tittel,
+        )
+    }
+}
---
+++ src/main/kotlin/no/nav/syfo/infrastructure/dokarkiv/dto/DokumentInfo.kt
@@ -0,0 +1,7 @@
+package no.nav.syfo.infrastructure.dokarkiv.dto
+
+data class DokumentInfo(
+    val brevkode: String? = null,
+    val dokumentInfoId: Int? = null,
+    val tittel: String? = null,
+)
