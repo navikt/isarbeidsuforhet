5a18052afe6d3288fe16048c59fa366d13de5557
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-02-27 15:29:40 +0100

    IS-2091: Add kafka environment and producer config (#24)

--- src/test/kotlin/no/nav/syfo/TestEnvironment.kt
+++ src/test/kotlin/no/nav/syfo/TestEnvironment.kt
@@ -5,6 +5,7 @@ import no.nav.syfo.infrastructure.ClientsEnvironment
 import no.nav.syfo.infrastructure.OpenClientEnvironment
 import no.nav.syfo.infrastructure.azuread.AzureEnvironment
 import no.nav.syfo.infrastructure.database.DatabaseEnvironment
+import no.nav.syfo.infrastructure.kafka.KafkaEnvironment
 
 fun testEnvironment() = Environment(
     database = DatabaseEnvironment(
@@ -13,6 +14,13 @@ fun testEnvironment() = Environment(
         name = "isaktivitetskrav_dev",
         username = "username",
         password = "password",
+    ),
+    kafka = KafkaEnvironment(
+        aivenBootstrapServers = "kafkaBootstrapServers",
+        aivenCredstorePassword = "credstorepassord",
+        aivenKeystoreLocation = "keystore",
+        aivenSecurityProtocol = "SSL",
+        aivenTruststoreLocation = "truststore",
     ),
     azure = AzureEnvironment(
         appClientId = "isarbeidsuforhet-client-id",
