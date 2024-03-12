55c8f50eac927660cc25b417de004eb3f05fb66a
Anders Rognstad <anders.rognstad@nav.no>
2024-03-12 15:09:05 +0100

    Fix valid journalpostId check

--- src/main/kotlin/no/nav/syfo/domain/JournalpostId.kt
+++ src/main/kotlin/no/nav/syfo/domain/JournalpostId.kt
@@ -3,7 +3,7 @@ package no.nav.syfo.domain
 @JvmInline
 value class JournalpostId(val value: String) {
     init {
-        if (value.toDoubleOrNull() == null) {
+        if (!value.all { it.isDigit() }) {
             throw IllegalArgumentException("Value is not a valid JournalpostId")
         }
     }
