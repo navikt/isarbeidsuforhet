0e14bee2c8fd2938cc10ac105102e5f219a9bd9a
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-04-09 13:45:26 +0200

    IS-2269: Increase journalforing frequency (#63)
    * IS-2269: Increase journalforing frequency
    
    * Refactor cronjob result logging

--- src/main/kotlin/no/nav/syfo/infrastructure/cronjob/PublishForhandsvarselCronjob.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/cronjob/PublishForhandsvarselCronjob.kt
@@ -1,8 +1,6 @@
 package no.nav.syfo.infrastructure.cronjob
 
-import net.logstash.logback.argument.StructuredArguments
 import no.nav.syfo.application.service.VarselService
-import org.slf4j.LoggerFactory
 
 class PublishForhandsvarselCronjob(
     private val varselService: VarselService,
@@ -10,19 +8,5 @@ class PublishForhandsvarselCronjob(
     override val initialDelayMinutes: Long = 4
     override val intervalDelayMinutes: Long = 10
 
-    override suspend fun run() {
-        val (success, failed) = varselService.publishUnpublishedVarsler().partition { it.isSuccess }
-        failed.forEach {
-            log.error("Exception caught while publishing forhandsvarsel", it.exceptionOrNull())
-        }
-        log.info(
-            "Completed publishing forhandsvarsel with result: {}, {}",
-            StructuredArguments.keyValue("failed", failed.size),
-            StructuredArguments.keyValue("updated", success.size),
-        )
-    }
-
-    companion object {
-        private val log = LoggerFactory.getLogger(PublishForhandsvarselCronjob::class.java)
-    }
+    override suspend fun run() = varselService.publishUnpublishedVarsler()
 }
