80ae09a73aeff42955a46298d7a951ba5a4ebf5e
Anders Rognstad <anders.rognstad@nav.no>
2024-04-09 09:14:20 +0200

    Refactor cronjob result logging

--- src/main/kotlin/no/nav/syfo/infrastructure/cronjob/JournalforVurderingerCronjob.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/cronjob/JournalforVurderingerCronjob.kt
@@ -1,8 +1,6 @@
 package no.nav.syfo.infrastructure.cronjob
 
-import net.logstash.logback.argument.StructuredArguments
 import no.nav.syfo.application.service.VurderingService
-import org.slf4j.LoggerFactory
 
 class JournalforVurderingerCronjob(
     private val vurderingService: VurderingService,
@@ -10,19 +8,5 @@ class JournalforVurderingerCronjob(
     override val initialDelayMinutes: Long = 2
     override val intervalDelayMinutes: Long = 1
 
-    override suspend fun run() {
-        val (success, failed) = vurderingService.journalforVurderinger().partition { it.isSuccess }
-        failed.forEach {
-            log.error("Exception caught while journalforing vurdering", it.exceptionOrNull())
-        }
-        log.info(
-            "Completed journalforing vurdering with result: {}, {}",
-            StructuredArguments.keyValue("failed", failed.size),
-            StructuredArguments.keyValue("updated", success.size),
-        )
-    }
-
-    companion object {
-        private val log = LoggerFactory.getLogger(JournalforVurderingerCronjob::class.java)
-    }
+    override suspend fun run() = vurderingService.journalforVurderinger()
 }
--- src/main/kotlin/no/nav/syfo/infrastructure/cronjob/PublishExpiredForhandsvarslerCronjob.kt
+++ src/main/kotlin/no/nav/syfo/infrastructure/cronjob/PublishExpiredForhandsvarslerCronjob.kt
@@ -1,8 +1,6 @@
 package no.nav.syfo.infrastructure.cronjob
 
-import net.logstash.logback.argument.StructuredArguments
 import no.nav.syfo.application.service.VarselService
-import org.slf4j.LoggerFactory
 
 class PublishExpiredForhandsvarslerCronjob(
     private val varselService: VarselService,
@@ -10,19 +8,5 @@ class PublishExpiredForhandsvarslerCronjob(
     override val initialDelayMinutes: Long = 3
     override val intervalDelayMinutes: Long = 10
 
-    override suspend fun run() {
-        val (success, failed) = varselService.publishExpiredForhandsvarsler().partition { it.isSuccess }
-        failed.forEach {
-            log.error("Exception caught while publishing expired forhandsvarsler", it.exceptionOrNull())
-        }
-        log.info(
-            "Completed publishing of expired forhandsvarsel with result: {}, {}",
-            StructuredArguments.keyValue("failed", failed.size),
-            StructuredArguments.keyValue("updated", success.size),
-        )
-    }
-
-    companion object {
-        private val log = LoggerFactory.getLogger(PublishExpiredForhandsvarslerCronjob::class.java)
-    }
+    override suspend fun run() = varselService.publishExpiredForhandsvarsler()
 }
